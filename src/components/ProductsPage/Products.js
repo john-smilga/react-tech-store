4283377800e8d1036075ecbcd427252601c7e5cc
John <johnsmilga@gmail.com>
2019-03-14 15:49:08 -0700

    Products Added To

---
+++ src/components/ProductsPage/Products.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { ProductConsumer } from "../../context";
+import styled from "styled-components";
+import Title from "../Title";
+import Product from "../Product";
+export default function Products() {
+  return (
+    <ProductConsumer>
+      {value => {
+        const { storeProducts } = value;
+        return (
+          <ProductsWrapper className="py-5">
+            <div className="container">
+              {/* title */}
+              <Title center title="our products" />
+              {/* products */}
+              <div className="row py-5">
+                {storeProducts.map(product => {
+                  return <Product key={product.id} product={product} />;
+                })}
+              </div>
+            </div>
+          </ProductsWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
+}
+
+const ProductsWrapper = styled.section``;
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
 import productsBcg from "../images/productsBcg.jpeg";
+import Products from "../components/ProductsPage/Products";
 export default function ProductsPage() {
-  return <Hero title="our products" img={productsBcg} />;
+  return (
+    <>
+      <Hero title="our products" img={productsBcg} />
+      <Products />
+    </>
+  );
 }
